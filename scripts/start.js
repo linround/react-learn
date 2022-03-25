83f17a964ce2aee8c615257722fcadeaef9646da
linyuan1105 <673609957@qq.com>
2022-03-25 14:12:51 +0800

    feature：环境更改

--- scripts/start.js
+++ scripts/start.js
@@ -44,7 +44,7 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 }
 
 // Tools like Cloud9 rely on this.
-const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 8000;
+const DEFAULT_PORT = 8888;
 const HOST = process.env.HOST || '0.0.0.0';
 console.log(chalk.cyan('===================chalk.cyan'))
 if (process.env.HOST) {
